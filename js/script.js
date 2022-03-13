6f7a55c6a91eb9444b2a0207f1abd7cab72820f3
AF1QUE <afique.dev@gmail.com>
2022-03-13 09:21:26 +0600

    Working on filter part

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
+// DOM Part
+
 const jobsContent = document.querySelector(".jobs");
-const jobsData = axios.get("../data.json");
+const filterGroup = document.querySelector(".filters");
 
 function getJobTags(job) {
   let tagsHTML = "";
   job.languages.forEach((item) => {
-    tagsHTML += `<span class="tag">${item}</span>`;
+    tagsHTML += `<span class="tag" data-id="${item}">${item}</span>`;
   });
   job.tools.forEach((item) => {
-    tagsHTML += `<span class="tag">${item}</span>`;
+    tagsHTML += `<span class="tag" data-id="${item}">${item}</span>`;
   });
+
+  tagsHTML += `<span class="tag" data-id="${job.role}">${job.role}</span>`;
+  tagsHTML += `<span class="tag" data-id="${job.level}">${job.level}</span>`;
 
   return tagsHTML;
 }
@@ -34,14 +39,14 @@ function getJobStatus(job) {
   return statusHTML;
 }
 
-jobsData.then((response) => {
+const jobsData = axios.get("../data.json").then((response) => {
   let jobHTML = "";
   response.data.map((job) => {
     let jobTags = getJobTags(job);
     let jobMeta = getJobMeta(job);
     let jobStatus = getJobStatus(job);
 
-    jobHTML += `<div class="job job--featured">
+    jobHTML += `<div class="job ${job.featured ? "job--featured" : ""}">
       <img src="${job.logo}" alt="" class="job__logo">
       <div class="job__content">
       <span class="job__content__company">${job.company}</span>
@@ -55,3 +60,44 @@ jobsData.then((response) => {
 
   jobsContent.innerHTML = jobHTML;
 });
+
+// Filter Part
+
+window.addEventListener("load", () => {
+  const tags = document.querySelectorAll(".tag");
+  tags.forEach((tag) => {
+    tag.addEventListener("click", (e) => {
+      const filterItem = e.currentTarget.dataset.id;
+      addFilters(filterItem);
+    });
+  });
+});
+
+let activeFilters = [];
+
+function addFilters(tagName) {
+  if (!activeFilters.includes(tagName)) {
+    activeFilters.push(tagName);
+  }
+
+  showFilters(activeFilters);
+}
+
+function showFilters(filters) {
+  let filtersHTML = "";
+  filters.forEach((filter) => {
+    filtersHTML += ` <div class="filter">
+        <button class="filter__title" data-id="${filter}">${filter}</button>
+        <div class="filter__remove"><img src="/images/icon-remove.svg" alt="remove button"></div>
+      </div>`;
+  });
+
+  filterGroup.innerHTML = filtersHTML;
+}
+
+// const removeBtn = document.querySelectorAll("filter__remove");
+// removeBtn.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     console.log("its working");
+//   });
+// });
